ee7eba138bf01e1b12395fc584efec1f93985717
arx8x <arx8@icloud.com>
2022-05-15 20:22:15 +0530

    terminate tasks together in runner

--- parallel_tasks/runner.py
+++ parallel_tasks/runner.py
@@ -14,6 +14,8 @@ class ParallelRunner:
         self.__max_parallel = max_parallel
         self.polling_rate = 0.5
         self.__failed_tasks = []
+        self.__task_pool = []
+        self.__stop_flag = False
 
     @property
     def failed_tasks(self):
@@ -24,24 +26,34 @@ class ParallelRunner:
         task_queue = self.__tasks.copy()
         # sort tasks based on priority
         task_queue = sorted(task_queue, key=lambda x: x.priority, reverse=True)
-        task_pool = []
         # run as long as there are active threads "and" tasks in the queue
-        while task_pool or task_queue:
+        while self.__task_pool or task_queue:
             # check for finished tasks and carry out post-exec tasks
             new_task_pool = []
-            for task in task_pool:
+            for task in self.__task_pool:
                 if task.did_complete:
                     if task.error:
                         self.__failed_tasks.append(task)
                 else:
                     new_task_pool.append(task)
                     # TODO: termination procedure
-            task_pool = new_task_pool
-            slots_left = self.__max_parallel - len(task_pool)
+            self.__task_pool = new_task_pool
+            if self.__stop_flag:
+                if not self.__task_pool:
+                    break
+                sleep(self.polling_rate)
+                continue
+            slots_left = self.__max_parallel - len(self.__task_pool)
             for _ in range(slots_left):
                 if not task_queue:
                     break
                 task = task_queue.pop(0)
-                task_pool.append(task)
+                self.__task_pool.append(task)
                 task.run()
             sleep(self.polling_rate)
+
+    def stop(self, force=False):
+        self.__stop_flag = True
+        if force:
+            for task in self.__task_pool:
+                task.stop()
